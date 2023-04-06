935afa83e39c63ce80712637ba490d33a9df1635
hmccarthy1 <huntermccarthy56@hotmail.com>
2023-04-06 17:05:20 -0400

    testing branch protection

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars' );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
