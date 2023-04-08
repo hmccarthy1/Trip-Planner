1d22f290be76dc75a588bde429a60584f161b7b6
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-04-08 16:09:54 -0400

    Imported 'user' in userRoutes, npm bcrypt, added index inside api controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,15 +5,4 @@ router.get('/', async (req, res) => {
     res.render('homepage', {})
 })
 
-router.get('/login', async (req, res) => {
-    res.render('login', {})
-})
-
-router.get('/register', async (req, res) => {
-    res.render('register')
-} )
-
-
-
-
 module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User');
+
+// User.hasMany(Blog, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Blog.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = { User };
