98c12aea5f6ffc0a13748144b0b3ceb4981bc86e
hmccarthy1 <huntermccarthy56@hotmail.com>
2023-04-16 14:37:12 -0400

    fixed youtube

--- public/js/spring.js
+++ public/js/spring.js
@@ -3,23 +3,23 @@ let player;
 function onYouTubeIframeAPIReady() {
 
     // Call your player initialization function here
+    
+    // Get the current URL
+    const currentUrl = window.location.href;
+    
+    // Extract the ID from the URL
+    const id = currentUrl.split('/').pop();
+    
+    const videoIndex = ['5V8cM9h4Hv4', '572Wde50ivs', '5J01oZPN_oA', 'DZywLrtbPlU', 'xNn_kek59j4', 'B-mtiykfHao', 'LaADC6I4Ofw', 'k57uK6aNNfg', 'tPzZ5JCfjs0', 'ibULOoGaA', 'Viytwxg0QRc', 'b9PXOXQzs-4'];
     initializePlayer();
-
-// Get the current URL
-const currentUrl = window.location.href;
-
-// Extract the ID from the URL
-const id = currentUrl.split('/').pop();
-
-videoIndex = ['5V8cM9h4Hv4', '572Wde50ivs', '5J01oZPN_oA', 'DZywLrtbPlU', 'xNn_kek59j4', 'B-mtiykfHao', 'LaADC6I4Ofw', 'k57uK6aNNfg', 'tPzZ5JCfjs0', 'ibULOoGaA', 'Viytwxg0QRc', 'b9PXOXQzs-4'];
-
+    
 function initializePlayer() {
   // Create and configure your player here
   var playerDiv = document.getElementById('player');
   var iframe = document.createElement('iframe');
   iframe.width = '470';
   iframe.height = '260';
-  iframe.src = 'https://www.youtube.com/embed/'+videoIndex[id-1];
+  iframe.src = 'https://www.youtube.com/embed/'+ videoIndex[id-1];
   iframe.frameBorder = '0';
   iframe.allowFullscreen = true;
   playerDiv.appendChild(iframe);
